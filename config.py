d15920f0676faa59766c7ad81d1a171af161c6b6
s1-cloud-demo <howiecloud25@gmail.com>
2026-02-12 11:51:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ AZURE_TENANT_ID="2ef92694-2c89-448d-ab1f-8db7a336d6c7"
 AZURE_CLIENT_SECRET="u8o8Q~R6vVJ-fjnHSlkz3wFYXQDrSeRWvxw6sa6q"
 
 ## GCP
-GOOGLE_API_KEY = 'AIzaSyBUPHAjZl3n8Eza66ka6B78iVyPteC5MgM'
+GOOGLE_API_KEY='AIzaSyBUPHAjZl3n8Eza66ka6B78iVyPteC5MgM'
 
 ################################################################################
 # AI
 ################################################################################
 
 ## Open AI
-OPENAI_API_KEY=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
+OPENAI_API_KEY='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'
 
 ################################################################################
 # Development
@@ -30,4 +30,12 @@ OPENAI_API_KEY=sk-proj-K8z_ky-1sxTcm6msgQLn7VDEUWwA6K8y5VOx_pyHPRCJ1mmpgexfiVrN-
 ## GitHub
 
 ## Google Captcha
-GOOGLE_CAPTCHA_KEY = '6Lrjv_b_jgnybWRwKSn2P6lop58PGZ_NfewZWnRT'
+GOOGLE_CAPTCHA_KEY='6Lrjv_b_jgnybWRwKSn2P6lop58PGZ_NfewZWnRT'
+
+
+################################################################################
+# Payment Services
+################################################################################
+
+## Stripe
+STRIPE_SECRET_KEY='sk_test_51L53TeSBwzby5YcBooVe7xqrw9DrV7SWyW5WKKYGSFO4fNSoDyd167DZrmIW6lZE6pHUDrYe9zfqGnCHTQUKhnPc00eL5w5pxY'
